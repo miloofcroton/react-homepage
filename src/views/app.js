2e859b8ad3ed0ef04388a9be402d924d711ceb96
Jack <ajackofsometrades@gmail.com>
2018-12-22 23:00:10 -0800

    organization

--- src/views/app.js
+++ src/views/app.js
@@ -1,14 +1,16 @@
 import React, { Fragment } from 'react';
+
 import { BrowserRouter, Switch, Redirect } from 'react-router-dom';
 import { routerRoutes, ROUTES } from './routes';
 
 import { Helmet } from 'react-helmet';
+
 import { ThemeProvider } from 'styled-components';
+import { theme } from './layout/Theme';
+import { GlobalStyle } from './layout/GlobalStyle';
 
 import Header from './layout/Header';
 import Footer from './layout/Footer';
-import { theme } from './layout/Theme';
-import { GlobalStyle } from './layout/GlobalStyle';
 
 const App = () => {
 
