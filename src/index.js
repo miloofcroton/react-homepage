590b06e8db1d1fa745c89bef5469dfd56fdfa7a8
Jack <ajackofsometrades@gmail.com>
2018-12-23 01:02:25 -0800

    concise naming

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import App from './views/app';
-import store from './data/state/store';
+import store from './data/store';
 
 render(
   <Provider store={store}>
